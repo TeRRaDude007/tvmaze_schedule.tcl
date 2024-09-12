15980a0fb541a44b302168a4bc4b7fbffe46bb79
TeRRaDude007 <180635713+TeRRaDude007@users.noreply.github.com>
2024-09-12 19:21:51 +0200

    Update tvmaze_schedule.tcl

--- tvmaze_schedule.tcl
+++ tvmaze_schedule.tcl
@@ -1,16 +1,14 @@
+bind pub - !tv pub:announce
+
 proc pub:announce {nick host handle chan arg} {
-    set time [clock format [clock seconds] -format "%H:%M"]
-    set country "US"
-    if {$arg eq "NL"} {
-        set country "NL"
-    }
-    set url "http://api.tvmaze.com/schedule?country=$country&start_time=$time"
+    set date [clock format [clock seconds] -format "%Y-%m-%d"]
+    set url "http://api.tvmaze.com/schedule?country=US&date=$date"
     set response [http::geturl $url]
     set data [http::data $response]
     set shows [json::json2dict $data]
     set count 0
     set skip_words { "Episode" "0 and 2."}
-    putquick "PRIVMSG $chan :  Here is the TV schedule for today:"
+    putquick "PRIVMSG $handle :Here is the TV schedule for today:"
     foreach show $shows {
         if {$count == 50} {
             after 2000
@@ -27,7 +25,7 @@ proc pub:announce {nick host handle chan arg} {
         set number [dict get $show "number"]
         set time [dict get $show "airtime"]
         if {[lsearch -exact $skip_words $name] == -1} {
-            putquick "PRIVMSG $chan :  $name \(S$season E$number\) airs at $time on $network"
+            putquick "PRIVMSG $handle :$name (S$season E$number) airs at $time on $network"
             incr count
         }
     }
