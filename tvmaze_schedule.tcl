63e0c64c6058823667748b83622bf8f52fa1e9b7
TeRRaDude007 <180635713+TeRRaDude007@users.noreply.github.com>
2024-09-12 19:21:03 +0200

    Update tvmaze_schedule.tcl

--- tvmaze_schedule.tcl
+++ tvmaze_schedule.tcl
@@ -1,20 +1,18 @@
-package require http
-package require json
-
-bind pub - !tv pub:announce
-
 proc pub:announce {nick host handle chan arg} {
-    set now [clock seconds]
-    set time [clock format $now -format "%H:%M"]
-    set url "http://api.tvmaze.com/schedule?country=US&start_time=$time"
+    set time [clock format [clock seconds] -format "%H:%M"]
+    set country "US"
+    if {$arg eq "NL"} {
+        set country "NL"
+    }
+    set url "http://api.tvmaze.com/schedule?country=$country&start_time=$time"
     set response [http::geturl $url]
     set data [http::data $response]
     set shows [json::json2dict $data]
     set count 0
     set skip_words { "Episode" "0 and 2."}
-    putquick "PRIVMSG $chan :  Here is the TV schedule for today starting from current Hour:"
+    putquick "PRIVMSG $chan :  Here is the TV schedule for today:"
     foreach show $shows {
-        if {$count == 10} {
+        if {$count == 50} {
             after 2000
         }
         set show_info [dict get $show "show"]
