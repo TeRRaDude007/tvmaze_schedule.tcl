f584a26f5fa0f0aa86248fde1cb5717a0f563de1
TeRRaDude007 <180635713+TeRRaDude007@users.noreply.github.com>
2024-09-12 19:20:28 +0200

    Update code

--- tvmaze_schedule.tcl
+++ tvmaze_schedule.tcl
@@ -4,16 +4,15 @@ package require json
 bind pub - !tv pub:announce
 
 proc pub:announce {nick host handle chan arg} {
-    #set date "2023-01-04"
     set now [clock seconds]
-    set date [clock format $now -format "%Y-%m-%d"]
-    set url "http://api.tvmaze.com/schedule?country=US&date=$date"
+    set time [clock format $now -format "%H:%M"]
+    set url "http://api.tvmaze.com/schedule?country=US&start_time=$time"
     set response [http::geturl $url]
     set data [http::data $response]
     set shows [json::json2dict $data]
     set count 0
-    set skip_words { "Episode" "Special" "Replay" }
-    putquick "PRIVMSG $chan :  Here is the TV schedule for today:"
+    set skip_words { "Episode" "0 and 2."}
+    putquick "PRIVMSG $chan :  Here is the TV schedule for today starting from current Hour:"
     foreach show $shows {
         if {$count == 10} {
             after 2000
@@ -21,7 +20,7 @@ proc pub:announce {nick host handle chan arg} {
         set show_info [dict get $show "show"]
         set name [dict get $show_info "name"]
         set network_info [dict get $show_info "network"]
-        if {$network_info eq ""} {
+        if {$network_info eq "null"} {
             set network "Not Available"
         } else {
             set network [dict get $network_info "name"]
@@ -30,7 +29,7 @@ proc pub:announce {nick host handle chan arg} {
         set number [dict get $show "number"]
         set time [dict get $show "airtime"]
         if {[lsearch -exact $skip_words $name] == -1} {
-            putquick "PRIVMSG $chan :  $name  on $network \(S$season E$number\) airs at $time"
+            putquick "PRIVMSG $chan :  $name \(S$season E$number\) airs at $time on $network"
             incr count
         }
     }
