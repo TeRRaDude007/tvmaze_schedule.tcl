aa6558d8c0076563246f4506b048effd34b9c93a
TeRRaDude007 <180635713+TeRRaDude007@users.noreply.github.com>
2024-09-14 21:16:21 +0200

    Update

--- tvmaze_auto_schedule.tcl
+++ tvmaze_auto_schedule.tcl
@@ -1,1 +1,11 @@
-
+# =====================================================
+# TeRRaDuDE TV
+# Version: v1.0.0
+# Last Updated: 2024-09-14
+# =====================================================
+#
+# v1.0.0 - 2024-09-14
+# - Extra Initial version witout a trigger.
+# - Announced the TV maze schedule every hour.
+# - Change #channelname where you want the output.
+# =====================================================
